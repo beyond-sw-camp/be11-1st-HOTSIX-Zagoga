7e748a934b1628eaccaac16b2e839e411f4031b4
ki-hyun-Jang <skjwj0511@gmail.com>
2024-12-02 21:11:13 +0900

    [feat] 인원수 및 선택한 도시 내에서 최저가 순으로 정렬 조회프로시저 추가

---
+++ PROCEDURE/인원수에 선택한 도시 내에서 최저가 순으로 정렬 조회.sql
@@ -0,0 +1,27 @@
+﻿-- 인원수에 선택한 도시 내에서 최저가 순으로 정렬 조회
+
+DELIMITER //
+
+CREATE PROCEDURE GetAccommodationsByCityAndPeople(
+    IN p_city VARCHAR(255),
+    IN p_num_people INT
+)
+BEGIN
+    SELECT 
+        a.id AS accommodation_id,
+        a.name AS accommodation_name,
+        r.type AS room_type,
+        r.off_peak_season_price,
+        r.people_max
+    FROM 
+        accommodation a
+    JOIN 
+        room r ON a.id = r.accommodation_id
+    WHERE 
+        a.address LIKE CONCAT('%', p_city, '%') 
+        AND r.people_max >= p_num_people 
+    ORDER BY 
+        r.off_peak_season_price ASC;
+END //
+
+DELIMITER ;

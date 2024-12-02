ea7788e1700f9f59adc1fa5b25c6de10c9bf63c7
gayeon99 <0916puppy@gmail.com>
2024-12-02 17:22:54 +0900

    db sql really final

--- testdata_1.sql
+++ testdata_1.sql
@@ -4,4 +4,9 @@
 -- owner insert
 
 -- admin insert
-
+alter table admin modify column type enum('sever_admin','custom_service');
+insert into admin(name, type) values('서버 관리자1','server_admin');
+insert into admin(name, type) values('서버 관리자2','server_admin');
+insert into admin(name, type) values('서버 관리자3','server_admin');
+insert into admin(name, type) values('상담원 John','server_admin');
+insert into admin(name, type) values('서버 관리자1','server_admin');

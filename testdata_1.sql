9db3bb9d7c1fbbd29347240fe82d60d99bda8d1f
ki-hyun-Jang <skjwj0511@gmail.com>
2024-12-02 18:53:32 +0900

    [feat] favorite_list 더미데이터 추가

--- testdata_1.sql
+++ testdata_1.sql
@@ -1,5 +1,5 @@
 -- user insert
-INSERT INTO user (name, personal_id, phone_number, email, sex, level, created_time, delete_user) VALUES
+INSERT INTO user (name, personal_id, phone_number, email, sex, level) VALUES
 ('홍길동', '123456-1234567', '010-1234-5678', 'hong@example.com', '남', 'Gold'),
 ('김영희', '234567-2345678', '010-2345-6789', 'kim@example.com', '여', 'Silver'),
 ('이철수', '345678-3456789', '010-3456-7890', 'lee@example.com', '남', 'Bronze'),
@@ -150,6 +150,15 @@ INSERT INTO accommodation_facility (accommodation_id, able_bbq, able_parking, ab
 (9, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE),
 (10, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, TRUE);
 
-
--- coupon insert
-insert into 
+-- favorite_list 즐겨찾기
+INSERT INTO favorite_list (user_id, accommodation_id) VALUES
+(1, 1),
+(2, 2),
+(1, 3),
+(3, 4),
+(2, 5),
+(4, 6),
+(1, 7),
+(5, 8),
+(3, 9),
+(2, 10);
